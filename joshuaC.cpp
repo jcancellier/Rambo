5cfcd0e10adaa62625bdee24f0541fdb810eaef1
Joshua Cancellier <sonicjcan@hotmail.com>
2018-02-24 03:23:03 -0800

    cleaned up code

--- joshuaC.cpp
+++ joshuaC.cpp
@@ -6,7 +6,7 @@
  *
  */
 
-#include<X11/keysym.h>
+#include <X11/keysym.h>
 #include "Global.h"
 #include "Timers.h"
 #include "SpriteSheet.h"
@@ -23,15 +23,16 @@ extern Character rambo;
 extern SpriteSheet img[];
 
 //Constructors
-Character::Character(int ssIdx){
-	centerX = 100;
-	centerY = 200;
-	height = 50;
-	width = height * 0.7;
-	frame = 0;
-	flipped = false;
+Character::Character(int ssIdx)
+{
+    centerX = 100;
+    centerY = 200;
+    height = 50;
+    width = height * 0.7;
+    frame = 0;
+    flipped = false;
     jumping = false;
-	health = 4;
+    health = 4;
     spriteSheetIndex = ssIdx;
     velocityX = 4;
     velocityY = 4;
@@ -58,73 +59,75 @@ void Character::setHealth(float h){health = h; }
 //Character Member functions
 void Character::draw()
 {
-	glPushMatrix();
-	glColor3f(1.0, 1.0, 1.0);
-	glBindTexture(GL_TEXTURE_2D, g.walkTexture);
-	//
-	glEnable(GL_ALPHA_TEST);
-	glAlphaFunc(GL_GREATER, 0.0f);
-	glColor4ub(255,255,255,255);
+    glPushMatrix();
+    glColor3f(1.0, 1.0, 1.0);
+    glBindTexture(GL_TEXTURE_2D, g.walkTexture);
+    glEnable(GL_ALPHA_TEST);
+    glAlphaFunc(GL_GREATER, 0.0f);
+    glColor4ub(255,255,255,255);
 
     float ssWidth = (float)1.0/img[spriteSheetIndex].columns;
     float ssHeight = (float)1.0/img[spriteSheetIndex].rows;
 
-	int ix = rambo.frame % img[spriteSheetIndex].columns;
-	int iy = 0;
+    int ix = rambo.frame % img[spriteSheetIndex].columns;
+    int iy = 0;
 
     //move to next row of spriteSheet (if available)
-	if(frame >= img[spriteSheetIndex].columns) {
+    if(frame >= img[spriteSheetIndex].columns) {
         iy = 1;
     }
 
-	float textureX = (float)ix / img[spriteSheetIndex].columns;
-	float textureY = (float)iy / img[spriteSheetIndex].rows;
-	glBegin(GL_QUADS);
-	    glTexCoord2f(textureX, textureY+ssHeight); 
-        glVertex2i(flipped ? centerX+width : centerX-width, centerY-height);
+    float textureX = (float)ix / img[spriteSheetIndex].columns;
+    float textureY = (float)iy / img[spriteSheetIndex].rows;
 
-	    glTexCoord2f(textureX, textureY);    
-        glVertex2i(flipped ? centerX+width : centerX-width, centerY+height);
+    glBegin(GL_QUADS);
+    glTexCoord2f(textureX, textureY+ssHeight); 
+    glVertex2i(flipped ? centerX+width : centerX-width, centerY-height);
 
-	    glTexCoord2f(textureX+ssWidth, textureY);    
-        glVertex2i(flipped ? centerX-width : centerX+width, centerY+height);
+    glTexCoord2f(textureX, textureY);    
+    glVertex2i(flipped ? centerX+width : centerX-width, centerY+height);
 
-	    glTexCoord2f(textureX+ssWidth, textureY+ssHeight); 
-        glVertex2i(flipped ? centerX-width : centerX+width, centerY-height);
-	glEnd();
-	glPopMatrix();
-	glBindTexture(GL_TEXTURE_2D, 0);
-	glDisable(GL_ALPHA_TEST); 
+    glTexCoord2f(textureX+ssWidth, textureY);    
+    glVertex2i(flipped ? centerX-width : centerX+width, centerY+height);
+
+    glTexCoord2f(textureX+ssWidth, textureY+ssHeight); 
+    glVertex2i(flipped ? centerX-width : centerX+width, centerY-height);
+    glEnd();
+
+    glPopMatrix();
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glDisable(GL_ALPHA_TEST); 
 }
 
 //Handle Input and animations
-void joshuaCInput(){
+void joshuaCInput()
+{
     if(keys[XK_Right] == 0 && keys[XK_Left] == 0 && !rambo.jumping){
         rambo.frame = 0;
     }
 
- 	jumpAnimation();
-	walkLeft();
+    jumpAnimation();
+    walkLeft();
 }
 
 void walkLeft()
 {
-	if(keys[XK_Left]){
-		rambo.flipped = true;
-		rambo.centerX -= rambo.velocityX;
-		timers.recordTime(&timers.timeCurrent);
-		//record time between frames
-		double timeSpan = timers.timeDiff(&timers.walkTime, 
-										&timers.timeCurrent);
-		if (timeSpan > g.delay) {
-			//advance frame
-			++rambo.frame;
-			if (rambo.frame >= 7) {
-				rambo.frame -= 6;
-			}
-			timers.recordTime(&timers.walkTime);
-		}
-	}
+    if (keys[XK_Left]) {
+        rambo.flipped = true;
+        rambo.centerX -= rambo.velocityX;
+        timers.recordTime(&timers.timeCurrent);
+        //record time between frames
+        double timeSpan = timers.timeDiff(&timers.walkTime, 
+                &timers.timeCurrent);
+        if (timeSpan > g.delay) {
+            //advance frame
+            ++rambo.frame;
+            if (rambo.frame >= 7) {
+                rambo.frame -= 6;
+            }
+            timers.recordTime(&timers.walkTime);
+        }
+    }
 }
 
 void jumpAnimation()
@@ -142,20 +145,20 @@ void jumpAnimation()
         //check if in walk state
         if (rambo.frame >= 0 && rambo.frame <= 6) {
             rambo.frame = 7;
-		}
+        }
         //rambo.flipped = (keys[XK_Left] ? true : false);
-        
-		timers.recordTime(&timers.timeCurrent);
-		//record time between frames
-		double timeSpan = timers.timeDiff(&timers.walkTime, 
-										&timers.timeCurrent);
-		if (timeSpan > g.delay) {
-			//advance frame
-			++rambo.frame;
-			if (rambo.frame >= 11) {
-				rambo.frame = 7;
-			}
-			timers.recordTime(&timers.walkTime);
-		}
+
+        timers.recordTime(&timers.timeCurrent);
+        //record time between frames
+        double timeSpan = timers.timeDiff(&timers.walkTime, 
+                &timers.timeCurrent);
+        if (timeSpan > g.delay) {
+            //advance frame
+            ++rambo.frame;
+            if (rambo.frame >= 11) {
+                rambo.frame = 7;
+            }
+            timers.recordTime(&timers.walkTime);
+        }
     }
 }
