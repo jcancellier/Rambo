82e2a916ba6d1e8bd2df01b4331265d856edf34a
Joshua Cancellier <joshuacancellier@f4-f-24-1-b-ba.dyn.csub.edu>
2018-03-22 12:00:34 -0700

    changes

--- walk.cpp
+++ walk.cpp
@@ -23,7 +23,7 @@
 #include "kuljitS.h"
 #include "fernandoM.h"
 #include "fonts.h"
-#include "Bullet.h" 
+#include "Bullet.h"
 #include "rafaelN.h"
 
 //GAME STATES
@@ -40,7 +40,7 @@ int nbullets = 0;
 int keys[65365];
 const float timeslice = 1.0f;
 float gravity = -0.5f;
-float cx = 100;    //Sprite x postion
+float cx = 100; //Sprite x postion
 float cy = 200; //Sprite y postion
 int flipped = 0;
 bool debug_mode = true;
@@ -61,88 +61,103 @@ Character rambo(0);
 //Setup timers
 Timers timers;
 
-
-class X11_wrapper {
-private:
+class X11_wrapper
+{
+  private:
     Display *dpy;
     Window win;
-public:
-    X11_wrapper() {
-        GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
+
+  public:
+    X11_wrapper()
+    {
+        GLint att[] = {GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None};
         //GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, None };
         XSetWindowAttributes swa;
         setupScreenRes(g.xres, g.yres);
         dpy = XOpenDisplay(NULL);
-        if (dpy == NULL) {
+        if (dpy == NULL)
+        {
             printf("\n\tcannot connect to X server\n\n");
             exit(EXIT_FAILURE);
         }
         Window root = DefaultRootWindow(dpy);
         XVisualInfo *vi = glXChooseVisual(dpy, 0, att);
-        if (vi == NULL) {
+        if (vi == NULL)
+        {
             printf("\n\tno appropriate visual found\n\n");
             exit(EXIT_FAILURE);
         }
         Colormap cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
         swa.colormap = cmap;
         swa.event_mask = ExposureMask | KeyPressMask | KeyReleaseMask |
-        StructureNotifyMask | SubstructureNotifyMask;
+                         StructureNotifyMask | SubstructureNotifyMask;
         win = XCreateWindow(dpy, root, 0, 0, g.xres, g.yres, 0,
                             vi->depth, InputOutput, vi->visual,
                             CWColormap | CWEventMask, &swa);
         GLXContext glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
         glXMakeCurrent(dpy, win, glc);
-        
+
         //disable autorepeat on key presses
         int mCheck;
         XkbSetDetectableAutoRepeat(dpy, false, &mCheck);
         setTitle();
     }
-    ~X11_wrapper() {
+    ~X11_wrapper()
+    {
         XDestroyWindow(dpy, win);
         XCloseDisplay(dpy);
     }
-    void setTitle() {
+    void setTitle()
+    {
         //Set the window title bar.
         XMapWindow(dpy, win);
         XStoreName(dpy, win, "Rambo");
     }
-    void setupScreenRes(const int w, const int h) {
+    void setupScreenRes(const int w, const int h)
+    {
         g.xres = w;
         g.yres = h;
     }
-    void reshapeWindow(int width, int height) {
+    void reshapeWindow(int width, int height)
+    {
         //window has been resized.
         setupScreenRes(width, height);
         glViewport(0, 0, (GLint)width, (GLint)height);
-        glMatrixMode(GL_PROJECTION); glLoadIdentity();
-        glMatrixMode(GL_MODELVIEW); glLoadIdentity();
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
         glOrtho(0, g.xres, 0, g.yres, -1, 1);
         setTitle();
     }
-    void checkResize(XEvent *e) {
+    void checkResize(XEvent *e)
+    {
         //The ConfigureNotify is sent by the
         //server if the window is resized.
         if (e->type != ConfigureNotify)
             return;
         XConfigureEvent xce = e->xconfigure;
-        if (xce.width != g.xres || xce.height != g.yres) {
+        if (xce.width != g.xres || xce.height != g.yres)
+        {
             //Window size did change.
             reshapeWindow(xce.width, xce.height);
         }
     }
-    bool getXPending() {
+    bool getXPending()
+    {
         return XPending(dpy);
     }
-    XEvent getXNextEvent() {
+    XEvent getXNextEvent()
+    {
         XEvent e;
         XNextEvent(dpy, &e);
         return e;
     }
-    void swapBuffers() {
+    void swapBuffers()
+    {
         glXSwapBuffers(dpy, win);
     }
-    
+
 } x11;
 
 //function prototypes
@@ -158,8 +173,10 @@ int main(void)
     initOpengl();
     init();
     int done = 0;
-    while (!done) {
-        while (x11.getXPending()) {
+    while (!done)
+    {
+        while (x11.getXPending())
+        {
             XEvent e = x11.getXNextEvent();
             x11.checkResize(&e);
             checkMouse(&e);
@@ -173,7 +190,6 @@ int main(void)
     return 0;
 }
 
-
 //This function removes background from sprite sheet
 unsigned char *buildAlphaData(SpriteSheet *img)
 {
@@ -183,24 +199,25 @@ unsigned char *buildAlphaData(SpriteSheet *img)
     unsigned char *data = (unsigned char *)img->data;
     newdata = (unsigned char *)malloc(img->width * img->height * 4);
     ptr = newdata;
-    unsigned char a,b,c;
+    unsigned char a, b, c;
     //use the first pixel in the image as the transparent color.
-    unsigned char t0 = *(data+0);
-    unsigned char t1 = *(data+1);
-    unsigned char t2 = *(data+2);
-    for (i=0; i<img->width * img->height * 3; i+=3) {
-        a = *(data+0);
-        b = *(data+1);
-        c = *(data+2);
-        *(ptr+0) = a;
-        *(ptr+1) = b;
-        *(ptr+2) = c;
-        *(ptr+3) = 1;
+    unsigned char t0 = *(data + 0);
+    unsigned char t1 = *(data + 1);
+    unsigned char t2 = *(data + 2);
+    for (i = 0; i < img->width * img->height * 3; i += 3)
+    {
+        a = *(data + 0);
+        b = *(data + 1);
+        c = *(data + 2);
+        *(ptr + 0) = a;
+        *(ptr + 1) = b;
+        *(ptr + 2) = c;
+        *(ptr + 3) = 1;
         //if pixel is color of background set the 4th component to 0
-        if (a==t0 && b==t1 && c==t2)
-            *(ptr+3) = 0;
+        if (a == t0 && b == t1 && c == t2)
+            *(ptr + 3) = 0;
         //-----------------------------------------------
-        
+
         //move to next pixel
         ptr += 4;
         data += 3;
@@ -213,8 +230,10 @@ void initOpengl(void)
     //OpenGL initialization
     glViewport(0, 0, g.xres, g.yres);
     //Initialize matrices
-    glMatrixMode(GL_PROJECTION); glLoadIdentity();
-    glMatrixMode(GL_MODELVIEW); glLoadIdentity();
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
     //This sets 2D mode (no perspective)
     glOrtho(0, g.xres, 0, g.yres, -1, 1);
     //
@@ -246,8 +265,8 @@ void initOpengl(void)
     //
     glBindTexture(GL_TEXTURE_2D, g.walkTexture);
     //
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     //
     //must build a new set of data...
     //This is where the texture is initialized in OpenGL (full sheet)
@@ -259,56 +278,63 @@ void initOpengl(void)
     //-------------------------------------------------------------------------
 }
 
-void init() {
-    
+void init()
+{
 }
 
 void checkMouse(XEvent *e)
 {
-    switch (gameState) {
-        case MAINMENU:
-            checkMouseMainMenu(e);  
-            break;
-        case INGAME:
-            //Did the mouse move?
-            //Was a mouse button clicked?
-            static int savex = 0;
-            static int savey = 0;
-            //
-            if (e->type == ButtonRelease) {
+    switch (gameState)
+    {
+    case MAINMENU:
+        checkMouseMainMenu(e);
+        break;
+    case INGAME:
+        //Did the mouse move?
+        //Was a mouse button clicked?
+        static int savex = 0;
+        static int savey = 0;
+        //
+        if (e->type == ButtonRelease)
+        {
             return;
-            }
-            if (e->type == ButtonPress) {
-            if (e->xbutton.button==1) {
+        }
+        if (e->type == ButtonPress)
+        {
+            if (e->xbutton.button == 1)
+            {
                 //Left button is down
             }
-            if (e->xbutton.button==3) {
+            if (e->xbutton.button == 3)
+            {
                 //Right button is down
             }
-            }
-            if (savex != e->xbutton.x || savey != e->xbutton.y) {
+        }
+        if (savex != e->xbutton.x || savey != e->xbutton.y)
+        {
             //Mouse moved
             savex = e->xbutton.x;
             savey = e->xbutton.y;
-            }
-            break;
-        default:
-            printf("Fatal Error in game state\n\n");
-            exit(1);
+        }
+        break;
+    default:
+        printf("Fatal Error in game state\n\n");
+        exit(1);
     }
 }
 
 int checkKeys(XEvent *e)
 {
-    
+
     int key = (XLookupKeysym(&e->xkey, 0) & 0x0000ffff);
-    switch (gameState) {
-        case MAINMENU:
-            return checkKeysMainMenu(key, e);
-            break;
-        case INGAME:
-            //keyboard input?
-            /*
+    switch (gameState)
+    {
+    case MAINMENU:
+        return checkKeysMainMenu(key, e);
+        break;
+    case INGAME:
+        //keyboard input?
+        /*
              static int shift=0;
              if (e->type != KeyRelease && e->type != KeyPress)
              return 0;
@@ -323,52 +349,65 @@ int checkKeys(XEvent *e)
              return 0;
              }
              */
-            
-            if(e->type == KeyPress){
-                keys[key] = 1;
-                //if(keys[XK_b])
-                //rambo.frame = 6;
-            }
-            if(e->type == KeyRelease){
-                keys[key] = 0;
-            }
 
-            if(e->type == KeyPress && key == XK_h){
-                debug_mode = !debug_mode;
-            }
-            
-            //(void)shift;
-            switch (key) {
-                case XK_w:
-                    timers.recordTime(&timers.walkTime);
-                    g.walk ^= 1;
-                    break;
-                case XK_Left:
-                    sound();
-                    break;
-                case XK_Right:
-                    break;
-                case XK_Up:
-                    break;
-                case XK_Down:
-                    break;
-                case XK_equal:
-                    g.delay -= 0.005;
-                    if (g.delay < 0.005)
-                        g.delay = 0.005;
-                    break;
-                case XK_minus:
-                    g.delay += 0.005;
-                    break;
-                case XK_Escape:
-                    return 1;
-                    break;
-            }
-            return 0;
+        if (e->type == KeyPress)
+        {
+            keys[key] = 1;
+            //if(keys[XK_b])
+            //rambo.frame = 6;
+        }
+        if (e->type == KeyRelease)
+        {
+            keys[key] = 0;
+        }
+
+        if (e->type == KeyPress && key == XK_h)
+        {
+            debug_mode = !debug_mode;
+        }
+
+        //(void)shift;
+        switch (key)
+        {
+        case XK_w:
+            timers.recordTime(&timers.walkTime);
+            g.walk ^= 1;
+            break;
+        case XK_Left:
+            sound();
+            break;
+        case XK_Right:
+            break;
+        case XK_Up:
+            break;
+        case XK_Down:
+            break;
+        case XK_equal:
+            g.delay -= 0.005;
+            if (g.delay < 0.005)
+                g.delay = 0.005;
+            break;
+        case XK_minus:
+            //
+            rambo.setHeight(rambo.getHeight() * .5);
+            rambo.setWidth(rambo.getWidth() * .5);
+            break;
+        case XK_u:
+            //
+            rambo.setHeight(rambo.getHeight() * 2);
+            rambo.setWidth(rambo.getWidth() * 2);
+            break;
+        case XK_Escape:
+            return 1;
             break;
         default:
-            printf("FATAL ERROR IN GAME STATE\n\n");
-            exit(1);
+            break;
+        }
+        return 0;
+        break;
+    default:
+        printf("FATAL ERROR IN GAME STATE\n\n");
+        exit(1);
     }
 
     return 0;
@@ -380,9 +419,10 @@ Flt VecNormalize(Vec vec)
     Flt xlen = vec[0];
     Flt ylen = vec[1];
     Flt zlen = vec[2];
-    len = xlen*xlen + ylen*ylen + zlen*zlen;
-    if (len == 0.0) {
-        MakeVector(0.0,0.0,1.0,vec);
+    len = xlen * xlen + ylen * ylen + zlen * zlen;
+    if (len == 0.0)
+    {
+        MakeVector(0.0, 0.0, 1.0, vec);
         return 1.0;
     }
     len = sqrt(len);
@@ -390,102 +430,104 @@ Flt VecNormalize(Vec vec)
     vec[0] = xlen * tlen;
     vec[1] = ylen * tlen;
     vec[2] = zlen * tlen;
-    return(len);
+    return (len);
 }
 
 void physics(void)
 {
-    switch (gameState) {
-        case MAINMENU:
-            break;
-        case INGAME:
-            joshuaCInput();
-            walkInput();
-           // spaceButton();
-            kuljitS_physics();
-            //fernandoPhysics();
-            break;
-        default:
-            printf("FATAL ERROR IN GAME STATE\n\n");
-            exit(1);
+    switch (gameState)
+    {
+    case MAINMENU:
+        break;
+    case INGAME:
+        joshuaCInput();
+        walkInput();
+        // spaceButton();
+        kuljitS_physics();
+        //fernandoPhysics();
+        break;
+    default:
+        printf("FATAL ERROR IN GAME STATE\n\n");
+        exit(1);
     }
 }
 
 void render(void)
 {
-    switch (gameState) {
-        case MAINMENU:
-            renderMainMenu();
-            break;
-        case INGAME: 
-            Rect r;
-            //Clear the screen
-            glClearColor(0.0, 0.5, 1.0, 1.0);
-            glClear(GL_COLOR_BUFFER_BIT);
-            
-            //the position of the sprite on the screen
-            //float cx = 100;
-            //float cy = 200;
-            //
-            //show ground
+    switch (gameState)
+    {
+    case MAINMENU:
+        renderMainMenu();
+        break;
+    case INGAME:
+        Rect r;
+        //Clear the screen
+        glClearColor(0.0, 0.5, 1.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //the position of the sprite on the screen
+        //float cx = 100;
+        //float cy = 200;
+        //
+        //show ground
+        glBegin(GL_QUADS);
+        glColor3f(0.0, 0.5, 0.0);
+        glVertex2i(0, 220);
+        glVertex2i(g.xres, 220);
+        glColor3f(0.0, 0.7, 0.0);
+        glVertex2i(g.xres, 0);
+        glVertex2i(0, 0);
+        glEnd();
+
+        for (int i = 0; i < 20; i++)
+        {
+            glPushMatrix();
+            glTranslated(g.box[i][0], g.box[i][1], g.box[i][2]);
+            glColor3f(1.0, 1.0, 1.0);
             glBegin(GL_QUADS);
-            glColor3f(0.0, 0.5, 0.0);
-            glVertex2i(0,       220);
-            glVertex2i(g.xres, 220);
-            glColor3f(0.0, 0.7, 0.0);
-            glVertex2i(g.xres,   0);
-            glVertex2i(0,         0);
+            glVertex2i(0, 0);
+            glVertex2i(0, 30);
+            glVertex2i(20, 30);
+            glVertex2i(20, 0);
             glEnd();
-            
-            for (int i=0; i<20; i++) {
-                glPushMatrix();
-                glTranslated(g.box[i][0],g.box[i][1],g.box[i][2]);
-                glColor3f(1.0, 1.0, 1.0);
-                glBegin(GL_QUADS);
-                glVertex2i( 0,  0);
-                glVertex2i( 0, 30);
-                glVertex2i(20, 30);
-                glVertex2i(20,  0);
-                glEnd();
-                glPopMatrix();
-            }
+            glPopMatrix();
+        }
 
         //    Bullet *b = new Bullet();
-            
-            //draw Rambo
-            rambo.draw();
-            rambo.drawOptimized();
-            //b->draw();
-            
 
-            {
-                Platform a(0, 0, 0, 20, 20, 20, 20, 0);
-                a.setColor(1, 0, 0);
-                a.drawPlatform();
-            }
-            //Rambo hitbox center
-            glPointSize(10);
-            glBegin(GL_POINTS);
-            glColor3f(0, 0, 0);
-            glVertex3f(rambo.getCenterX(), rambo.getCenterY(), 0);
-            glEnd();
-            
-            if(debug_mode){
-                unsigned int c = 0x00ffff44;
-                r.bot = g.yres - 20;
-                r.left = 10;
-                r.center = 0;
-                ggprint8b(&r, 16, c, "right arrow -> walk right");
-                ggprint8b(&r, 16, c, "left arrow  <- walk left");
-                ggprint8b(&r, 16, c, "a key to jump");
-                ggprint8b(&r, 16, c, "h key to toggle debug mode");
-                printKuljitS(g.xres - 100, g.yres-20, 16, 0);
-                printFernandoM(12, 0);    
-            }
-            break;
-        default:
-            printf("FATAL ERROR IN GAME STATE\n\n");
-            exit(1);
+        //draw Rambo
+        rambo.draw();
+        rambo.drawOptimized();
+        //b->draw();
+
+        {
+            Platform a(0, 0, 0, 20, 20, 20, 20, 0);
+            a.setColor(1, 0, 0);
+            a.drawPlatform();
+        }
+        //Rambo hitbox center
+        glPointSize(10);
+        glBegin(GL_POINTS);
+        glColor3f(0, 0, 0);
+        glVertex3f(rambo.getCenterX(), rambo.getCenterY(), 0);
+        glEnd();
+
+        if (debug_mode)
+        {
+            unsigned int c = 0x00ffff44;
+            r.bot = g.yres - 20;
+            r.left = 10;
+            r.center = 0;
+            ggprint8b(&r, 16, c, "right arrow -> walk right");
+            ggprint8b(&r, 16, c, "left arrow  <- walk left");
+            ggprint8b(&r, 16, c, "a key to jump");
+            ggprint8b(&r, 16, c, "h key to toggle debug mode");
+            printKuljitS(g.xres - 100, g.yres - 20, 16, 0);
+            printFernandoM(12, 0);
+        }
+        break;
+    default:
+        printf("FATAL ERROR IN GAME STATE\n\n");
+        exit(1);
     }
 }
-
